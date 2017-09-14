57d8a1ad35a5237ad986ae30a033452817ae8c85
Raidel Berrillo Gonzalez <raidelbg84@gmail.com>
2017-09-13 23:42:47 -0500

    Reservacion

--- resources/views/index.php
+++ resources/views/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>SISTEMA RESERVA NAZCAR</title>
 
     <link href="<?= asset('../lib/bootstrap-4/dist/css/bootstrap.min.css') ?>" rel="stylesheet">
     <link href="<?= asset('../lib/bootstrap-datetimepicker/bootstrap-datetimepicker.min.css') ?>" rel="stylesheet">
@@ -43,8 +43,8 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#rent">Rentas <span class="sr-only">(current)</span></a>
+            <li class="nav-item">
+                <a class="nav-link" href="#rent">Rentas</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#client">Clientes</a>
