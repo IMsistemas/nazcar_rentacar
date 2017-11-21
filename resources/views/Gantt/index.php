a590d931f294036f965d9afc33742c65db5d94c0
MSc Raidel Berrillo Gonzalez <raidelbg84@gmail.com>
2017-11-21 10:55:46 -0500

    Reservacion

--- resources/views/Gantt/index.php
+++ resources/views/Gantt/index.php
@@ -4,14 +4,14 @@
     }*/
 </style>
 
-<div class="row" style="margin-top: 10px;" ng-controller="ganttController" ng-init="initLoad()">
+<div class="container" style="margin-top: 10px;" ng-controller="ganttController" ng-init="initLoad()">
 
     <div class="col-xs-12">
         <h4>Disponibilidad - Diagrama de Gantt</h4>
         <hr>
     </div>
 
-    <div class="col-xs-12" style="position:relative;" class="container gantt" id="GanttChartDIV"></div>
+    <div style="position:relative;" class="container gantt" id="GanttChartDIV"></div>
 
 </div>
 
